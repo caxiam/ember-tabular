422a1fddc4a50ca2c4d4908ebc5b0e56fd235785
Aaron Fischer <aaron.fischer@caxiam.com>
2016-03-01 14:47:11 -0500

    Add simple alert component
    Reference issue #1

---
+++ addon/components/ember-table-jsonapi-alert.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: 'div',
+    type: 'info',
+    typeClass: Ember.computed('type', function() {
+        return 'alert-' + this.get('type');
+    }),
+});

8fb2ad69660d78414779da17d2c137ba97a7b620
Aaron Fischer <aaron.fischer@caxiam.com>
2016-03-07 16:06:02 -0500

    Update dummy testing to properly reference application and seed data
    Reference issue #1

--- tests/acceptance/basic-test.js
+++ tests/acceptance/basic-test.js
@@ -1,14 +1,8 @@
 import moduleForAcceptance from '../helpers/module-for-acceptance';
 import { test } from 'qunit';
-import startMirage from '../helpers/setup-mirage-for-integration';
-
-var application;
 
 moduleForAcceptance('Acceptance: Simple Table', {
     integration: true,
-    setup: function() {
-        startMirage(this.container);
-    }
 });
 
 test('Check for expected content /', function(assert) {
@@ -21,7 +15,7 @@ test('Check for expected content /', function(assert) {
         var cells = find('table tbody tr').eq(0).find('td');
 
         assert.equal(cells.eq(0).html(), 'YippieKiYay', 'Check for username');
-        assert.equal(cells.eq(1).html(), 'john.mcclane@caxiam.com', 'Check for email');
+        assert.equal(cells.eq(1).html(), 'john.mcclane@domain.com', 'Check for email');
         assert.equal(cells.eq(2).html(), 'John', 'Check for first name');
         assert.equal(cells.eq(3).html(), 'McClane', 'Check for last name');
         assert.equal(cells.eq(4).text().trim(), '01/02/2017', 'Check for date');
@@ -36,7 +30,7 @@ test('Check table rendering of / for no data or loading', function(assert) {
         assert.equal(currentPath(), 'index');
     });
 
-    var store = application.__container__.lookup('service:store');
+    var store = this.application.__container__.lookup('service:store');
     andThen(function() {
         store.unloadAll('user');
     });
--- tests/dummy/mirage/fixtures/users.js
+++ tests/dummy/mirage/fixtures/users.js
@@ -3,22 +3,22 @@ export default [
         'id': 1,
         'type': 'users',
         'username': 'YippieKiYay',
-        'email-address': 'john.mcclane@domain.com',
-        'first-name': 'John',
-        'last-name': 'McClane',
-        'is-admin': true,
-        'created-at': '2017-01-01',
-        'updated-at': '2017-01-02'
+        'emailAddress': 'john.mcclane@domain.com',
+        'firstName': 'John',
+        'lastName': 'McClane',
+        'isAdmin': true,
+        'createdAt': '2017-01-01',
+        'updatedAt': '2017-01-02'
     },
     {
         'id': 2,
         'type': 'users',
         'username': 'YippieKiYay',
-        'email-address': 'john.mcclane@domain.com',
-        'first-name': 'John',
-        'last-name': 'McClane',
-        'is-admin': true,
-        'created-at': '2017-01-01',
-        'updated-at': '2017-01-02'
+        'emailAddress': 'john.mcclane@domain.com',
+        'firstName': 'John',
+        'lastName': 'McClane',
+        'isAdmin': true,
+        'createdAt': '2017-01-01',
+        'updatedAt': '2017-01-02'
     }
 ];

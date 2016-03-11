a48ece658ad927a2c4a411204af6a15bed8f7b7f
Aaron Fischer <aaron.fischer@caxiam.com>
2016-03-11 14:54:41 -0500

    Add CSS to component required

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-table-jsonapi'
+  name: 'ember-table-jsonapi',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import('vendor/app.css');
+  }
 };

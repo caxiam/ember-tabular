388fca6766cc45639219b289d98e7c44ba1de025
Aaron Fischer <aaron.fischer@caxiam.com>
2016-07-11 14:47:22 -0400

    Add comment blocks for dropdown filter component
    Reference issue #53

--- addon/components/ember-tabular-dropdown-filter.js
+++ addon/components/ember-tabular-dropdown-filter.js
@@ -1,3 +1,7 @@
 import EmberTabularGlobalFilter from './ember-tabular-global-filter';
 
+/**
+* @class EmberTabularDropdownFilter
+* @extends EmberTabularGlobalFilter
+*/
 export default EmberTabularGlobalFilter.extend();

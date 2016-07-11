f2deae1751841d9abdf46f7c2af2f3ecccf86de1
Aaron Fischer <aaron.fischer@caxiam.com>
2016-07-11 14:46:00 -0400

    Add comment blocks for alert component
    Reference issue #53

--- addon/components/ember-tabular-alert.js
+++ addon/components/ember-tabular-alert.js
@@ -1,8 +1,26 @@
 import Ember from 'ember';
 
+/**
+* @class EmberTabularAlert
+*/
 export default Ember.Component.extend({
+  /**
+  * @property tagName
+  * @type String
+  * @default 'div'
+  */
   tagName: 'div',
+  /**
+  * @property type
+  * @type String
+  * @default 'info'
+  */
   type: 'info',
+  /**
+  * @property typeClass
+  * @type String
+  * @default 'alert-[type]'
+  */
   typeClass: Ember.computed('type', function () {
     return `alert-${this.get('type')}`;
   }),

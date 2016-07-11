af8b5a2da564e5362d86e01b755e6219698721b1
Aaron Fischer <aaron.fischer@caxiam.com>
2016-07-11 14:47:14 -0400

    Add comment blocks for date filter component
    Reference issue #53

--- addon/components/ember-tabular-date-filter.js
+++ addon/components/ember-tabular-date-filter.js
@@ -1,13 +1,49 @@
 import Ember from 'ember';
 
+/**
+* @class EmberTabularDateFilter
+*/
 export default Ember.Component.extend({
+  /**
+  * @property tagName
+  * @type String
+  * @default 'div'
+  */
   tagName: 'div',
   classNames: ['table-filter'],
   action: null,
+  /**
+  * Property to be filtered upon.
+  *
+  * @property filterProperty
+  * @type String
+  * @default null
+  */
   filterProperty: null,
+  /**
+  * Value of filter.
+  *
+  * @property dateFilter
+  * @type String
+  * @default ''
+  */
   dateFilter: '',
 
+  /**
+  * Pass the `query` object from the parent component if it is different or if used outside of the context of the component, otherwise `query` is optional and the component will attempt to grab within the context of the parent component.
+  *
+  * @property query
+  * @type Object
+  * @default null
+  */
   query: null,
+  /**
+  * Must expose the `filter` property on the parent ember-tabular component to be able to pass the filter object back and forth between parent and child components.
+  *
+  * @property filter
+  * @type Object
+  * @default null
+  */
   filter: null,
 
   actions: {
@@ -15,15 +51,28 @@ export default Ember.Component.extend({
       this.set('dateFilter', '');
     },
   },
+  /**
+  * Debounce the `filterName` method.
+  *
+  * @method filterTable
+  */
   filterTable: Ember.observer('dateFilter', function () {
     Ember.run.debounce(this, 'filterName', 750);
   }),
+  /**
+  * @property isClearable
+  */
   isClearable: Ember.computed('dateFilter', function () {
     if (this.get('dateFilter')) {
       return true;
     }
     return false;
   }),
+  /**
+  * Constructs and sets the `filter` Object.
+  *
+  * @method filterName
+  */
   filterName() {
     // Reference parent component query obj
     const query = this.get('query') || this.get('parentView.query');

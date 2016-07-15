3c1fa7be163abf3f9b17bf68ab839c543a8f7a5e
Aaron Fischer <aaron.fischer@caxiam.com>
2016-07-15 10:21:10 -0400

    Commit /docs/* directory demo site access

---
+++ docs/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EmberTabular",
+        "EmberTabularAlert",
+        "EmberTabularDateFilter",
+        "EmberTabularDropdownFilter",
+        "EmberTabularFilter",
+        "EmberTabularGlobalFilter"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});

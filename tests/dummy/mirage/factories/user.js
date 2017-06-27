f5942c1ab66fcaca824ad37dbf8b9edba9b09b7a
Aaron Fischer <aaron.fischer@caxiam.com>
2017-06-27 12:07:55 -0400

    Fix mock user data

--- tests/dummy/mirage/factories/user.js
+++ tests/dummy/mirage/factories/user.js
@@ -2,11 +2,12 @@ import Mirage, {faker}  from 'ember-cli-mirage';
 import moment from 'moment';
 
 export default Mirage.Factory.extend({
-  'username'() { return faker.internet.userName(); },
-  'emailAddress'() { return faker.internet.email(); },
-  'firstName'() { return faker.name.firstName(); },
-  'lastName'() { return faker.name.lastName(); },
-  'isAdmin'() { return faker.random.arrayElement([true, false]); },
-  'createdAt'() { return moment(faker.date.past()).format('YYYY-MM-DD'); },
-  'updatedAt'() { return moment(faker.date.past()).format('YYYY-MM-DD'); },
+  username() { return faker.internet.userName(); },
+  emailAddress() { return faker.internet.email(); },
+  password() { return faker.internet.password(); },
+  firstName() { return faker.name.firstName(); },
+  lastName() { return faker.name.lastName(); },
+  isAdmin() { return faker.random.arrayElement([true, false]); },
+  createdAt() { return moment(faker.date.past()).format('YYYY-MM-DD'); },
+  updatedAt() { return moment(faker.date.past()).format('YYYY-MM-DD'); },
 });

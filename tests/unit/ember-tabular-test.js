160cf1af6ba460b71525742e807a9cb694a09ee9
Aaron Fischer <aaron.fischer@caxiam.com>
2017-10-12 23:51:24 -0400

    Resolve remaining ember test issues, ensure all services/components are set within the unit test

--- tests/unit/ember-tabular-test.js
+++ tests/unit/ember-tabular-test.js
@@ -63,7 +63,7 @@ let columns = [
 
 moduleForComponent('ember-tabular', 'Unit | Component | ember table jsonapi', {
   unit: true,
-  needs: ['helper:dot-to-dash-filter', 'component:ember-tabular-filter', 'component:ember-tabular-footer', 'helper:and', 'component:pagination-pager', 'template:components/ember-tabular-row', 'component:ember-tabular-column-select', 'helper:compare-with', 'component:ember-tabular-column'],
+  needs: ['helper:dot-to-dash-filter', 'component:ember-tabular-filter', 'component:ember-tabular-footer', 'helper:and', 'component:pagination-pager', 'component:ember-tabular-column-select', 'component:sortable-objects', 'service:dragCoordinator', 'component:draggable-object', 'helper:compare-with', 'component:ember-tabular-column'],
   beforeEach: function() {
     component = this.subject({
       record: model,

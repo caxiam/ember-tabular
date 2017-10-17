662ffdf072d104c900f71e3a101e402ad56b9503
Aaron Fischer <aaron.fischer@caxiam.com>
2017-10-17 15:42:08 -0400

    Upgrade to ember-cli 2.15.1, use Chrome as the preferred test

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,13 @@
 /* eslint-env node */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
+'use strict';
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+let Funnel = require('broccoli-funnel');
+let mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
-  var environment = process.env.EMBER_ENV;
+  let environment = process.env.EMBER_ENV;
 
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     'ember-cli-babel': {
       includePolyfill: environment === 'test',
     },
@@ -25,7 +26,7 @@ module.exports = function(defaults) {
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
   // Copy non-compiled bootstrap dependency
-  var vendor = new Funnel('bower_components/bootstrap/dist/css/', {
+  let vendor = new Funnel('bower_components/bootstrap/dist/css/', {
     srcDir: '/',
     include: ['bootstrap.css.map'],
     destDir: '/assets'

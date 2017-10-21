353d02dffc8491439ca50c2adac2095fba1b7328
Aaron Fischer <aaron.fischer@caxiam.com>
2017-10-21 13:23:00 -0400

    Do not duplicate dependencies

--- blueprints/ember-tabular/index.js
+++ blueprints/ember-tabular/index.js
@@ -1,12 +1,3 @@
 module.exports = {
   normalizeEntityName() {},
-
-  afterInstall() {
-    return this.addBowerPackagesToProject([
-      {
-        name: 'font-awesome',
-        target: '4.6.2',
-      },
-    ]);
-  },
 };

b8d4768258a058118badc88ac16f1105b070df0b
Aaron Fischer <aaron.fischer@caxiam.com>
2017-10-22 23:57:28 -0400

    Fix testing for urls

--- tests/acceptance/relationships-test.js
+++ tests/acceptance/relationships-test.js
@@ -28,7 +28,7 @@ test('Check for expected url when filtering relationships', function(assert) {
 
   andThen(function() {
     click('.table-override-columns-template table .btn-toggle-filter:eq(0)');
-    fillIn('.table-override-columns-template table thead tr:eq(1) th:eq(0) input', 'Testing Relationships');
+    fillIn('.table-override-columns-template table thead tr:eq(1) th:eq(0) input', 'Testing');
     find('.table-override-columns-template table thead tr:eq(1) th:eq(0) input').trigger('keyup');
   });
 
@@ -37,7 +37,7 @@ test('Check for expected url when filtering relationships', function(assert) {
 
     assert.equal(request.status, 200);
     assert.equal(request.method, 'GET');
-    assert.equal(request.url, '/users?filter%5Baccount.username%5D=Testing%20Relationships&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=username', 'URL has relationship in dot notation');
+    assert.equal(request.url, '/users?filter%5Baccount.username%5D=Testing&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=username', 'URL has relationship in dot notation');
   });
 
   andThen(function() {
@@ -47,7 +47,7 @@ test('Check for expected url when filtering relationships', function(assert) {
 
   andThen(function() {
     // make another request to the relationship
-    fillIn('.table-override-columns-template table thead tr:eq(1) th:eq(0) input', 'Testing Relationships 2');
+    fillIn('.table-override-columns-template table thead tr:eq(1) th:eq(0) input', 'Testing2');
     find('.table-override-columns-template table thead tr:eq(1) th:eq(0) input').trigger('keyup');
   });
 
@@ -56,6 +56,6 @@ test('Check for expected url when filtering relationships', function(assert) {
 
     assert.equal(request.status, 200);
     assert.equal(request.method, 'GET');
-    assert.equal(request.url, '/users?filter%5Baccount.username%5D=Testing%20Relationships%202&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=username', 'URL has retained dot notation');
+    assert.equal(request.url, '/users?filter%5Baccount.username%5D=Testing2&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=username', 'URL has retained dot notation');
   });
 });

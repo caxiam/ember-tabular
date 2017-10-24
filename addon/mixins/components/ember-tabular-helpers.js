2043d950d82eb08a8ab2758cf2c8744f95b28abc
Aaron Fischer <aaron.fischer@caxiam.com>
2017-10-23 22:53:32 -0400

    Improve formatColumnLabel, to format dot notation

--- addon/mixins/components/ember-tabular-helpers.js
+++ addon/mixins/components/ember-tabular-helpers.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
   _formatColumnLabel(text) {
-    let result = text.replace(/([A-Z])/g, ' $1');
+    let results = text.split('.');
+    // ensure first character is capitalized
+    let result = results.map((str) => {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }).join(' ');
+    result = result.replace(/([A-Z])/g, ' $1');
     return result.charAt(0).toUpperCase() + result.slice(1);
   },
 });

870318a39cc441c54a94af10bb5215bb1f7ee485
Aaron Fischer <aaron.fischer@caxiam.com>
2019-01-24 11:45:02 -0500

    Attempt upgrade to circleci 2.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,28 +2,6 @@
 module.exports = {
   scenarios: [
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      },
-      npm: {
-        dependencies: {
-          'ember-font-awesome': null
-        },
-        peerDependencies: {
-          'ember-font-awesome': '^3.0.0'
-        },
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
@@ -53,6 +31,22 @@ module.exports = {
         },
         resolutions: {
           'ember': 'lts-2-12'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16.2',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#2.16.2'
+        },
+        resolutions: {
+          'ember': 'lts-2-16'
         }
       },
       npm: {

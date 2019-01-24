2a58a3a5cfb37ba83f049c5a4fbd0affd10768d3
Aaron Fischer <aaron.fischer@caxiam.com>
2019-01-23 23:40:50 -0500

    Upgrade test suite to ember-qunit (support new syntax)
    Reference issue #107

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,14 +2,17 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 import util from './util';
-import registerPowerSelectHelpers from '../../tests/helpers/ember-power-select';
+import registerPowerSelectHelpers from 'ember-power-select/test-support/helpers';
+import registerBasicDropdownHelpers from 'ember-basic-dropdown/test-support/helpers';
 
 // Initialize helpers
 util();
 registerPowerSelectHelpers();
+registerBasicDropdownHelpers();
 
 export default function startApp(attrs) {
   let attributes = Ember.merge({}, config.APP);
+  attributes.autoboot = true;
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   return Ember.run(() => {

b05307bff4fa44e6fc9996408bd5dea6dda2ceb7
Aaron Fischer <aaron.fischer@caxiam.com>
2019-01-24 11:31:10 -0500

    Fix unit testing

--- tests/unit/ember-tabular-test.js
+++ tests/unit/ember-tabular-test.js
@@ -1,88 +1,79 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
-
-let component;
-let model = [
-  {
-    id: 1,
-    username: 'YippieKiYay',
-    emailAddress: 'john.mcclane@domain.com',
-    firstName: 'John',
-    lastName: 'McClane',
-    isAdmin: true,
-    createdAt: '2017-01-01',
-    updatedAt: '2017-01-02'
-  }, {
-    id: 2,
-    username: 'HanSolo',
-    emailAddress: 'smugglerOne@domain.com',
-    firstName: 'Han',
-    lastName: 'Solo',
-    isAdmin: false,
-    createdAt: '2015-12-18',
-    updatedAt: '2015-12-19'
-  }
-];
-let columns = [
-  {
-    property: 'username',
-    label: 'Username',
-    isActive: true,
-    type: 'text',
-    defaultSort: 'username',
-  },
-  {
-    property: 'email-address',
-    label: 'Email',
-    isActive: true,
-    type: 'text',
-  },
-  {
-    property: 'first-name',
-    label: 'First Name',
-    isActive: true,
-    type: 'text',
-  },
-  {
-    property: 'last-name',
-    label: 'Last Name',
-    isActive: true,
-    type: 'text',
-  },
-  {
-    property: 'updated-at',
-    label: 'Last Updated',
-    isActive: true,
-    type: 'date',
-  },
-  {
-    property: 'actions',
-    label: 'Actions',
-    isActive: true,
-  },
-];
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Unit | Component | ember table jsonapi', function(hooks) {
-  setupTest(hooks);
+  setupRenderingTest(hooks);
 
-  hooks.beforeEach(function() {
-    component = this.owner.factoryFor('component:ember-tabular').create({
-      record: model,
-      makeRequest: false,
-      columns: columns,
-      isDropdownLimit: false,
-    });
+  test('it renders', async function(assert) {
+    await render(hbs`{{ember-tabular makeRequest=false}}`);
+
+    let $component = this.$();
+    assert.equal($component.length, 1, 'It renders in DOM');
   });
 
-  test('it renders', function(assert) {
-    assert.expect(2);
-    assert.equal(component._state, 'preRender', 'It pre-rendered');
-    this.render();
-    assert.equal(component._state, 'inDOM', 'It is in the DOM');
-  });
-
-  test('it displays table headers', function(assert) {
-    this.render();
+  test('it displays table headers', async function(assert) {
+    this.model = [
+      {
+        id: 1,
+        username: 'YippieKiYay',
+        emailAddress: 'john.mcclane@domain.com',
+        firstName: 'John',
+        lastName: 'McClane',
+        isAdmin: true,
+        createdAt: '2017-01-01',
+        updatedAt: '2017-01-02'
+      }, {
+        id: 2,
+        username: 'HanSolo',
+        emailAddress: 'smugglerOne@domain.com',
+        firstName: 'Han',
+        lastName: 'Solo',
+        isAdmin: false,
+        createdAt: '2015-12-18',
+        updatedAt: '2015-12-19'
+      }
+    ];
+    this.columns = [
+      {
+        property: 'username',
+        label: 'Username',
+        isActive: true,
+        type: 'text',
+        defaultSort: 'username',
+      },
+      {
+        property: 'email-address',
+        label: 'Email',
+        isActive: true,
+        type: 'text',
+      },
+      {
+        property: 'first-name',
+        label: 'First Name',
+        isActive: true,
+        type: 'text',
+      },
+      {
+        property: 'last-name',
+        label: 'Last Name',
+        isActive: true,
+        type: 'text',
+      },
+      {
+        property: 'updated-at',
+        label: 'Last Updated',
+        isActive: true,
+        type: 'date',
+      },
+      {
+        property: 'actions',
+        label: 'Actions',
+        isActive: true,
+      },
+    ];
+    await render(hbs`{{ember-tabular record=model makeRequest=false columns=columns isDropdownLimit=false}}`);
 
     var $component = this.$();
     assert.equal($component.find('thead tr:eq(0) th:eq(0)').text().trim(), 'Username', 'Table Filter - Username');
